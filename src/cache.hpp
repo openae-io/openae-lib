b034e37fb4844ddb39b82fbaee2e2c1ee9b3e9d6
Lukas Berbuer <lukas.berbuer@gmail.com>
2024-12-24 14:48:34 +0100

    feat: RingBufferStorage for Cahce

--- src/cache.hpp
+++ src/cache.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <cstddef>
 #include <functional>  // invoke
 #include <optional>
@@ -17,33 +18,76 @@ struct CacheKey {
     auto operator<=>(const CacheKey&) const = default;
 };
 
-template <typename T>
-class SingleCacheEntryStorage {
+template <typename Key, typename T>
+class SingleEntryStorage {
 public:
-    T& insert(CacheKey key, const T& result) noexcept(std::is_nothrow_copy_assignable_v<T>) {
-        entry_.emplace(key, result);
-        return entry_->value;
-    }
-
-    T& insert(CacheKey key, T&& result) noexcept(std::is_nothrow_move_assignable_v<T>) {
+    T& insert(Key key, T result) {
         entry_.emplace(key, std::move(result));
-        return entry_->value;
+        return entry_->second;
     }
 
-    const T* find(CacheKey key) const noexcept {
-        if (entry_.has_value() && entry_->key == key) {
-            return &entry_->value;
+    const T* find(Key key) const noexcept {
+        if (entry_.has_value() && entry_->first == key) {
+            return &entry_->second;
         }
         return nullptr;
     }
 
 private:
-    struct Entry {
-        CacheKey key;
-        T value;
-    };
+    std::optional<std::pair<Key, T>> entry_;
+};
 
-    std::optional<Entry> entry_;
+template <typename Key, typename T, std::size_t N>
+class RingBufferStorage {
+public:
+    std::size_t size() const noexcept {
+        return size_;
+    }
+
+    T& insert(Key key, T result) {
+        if (auto* existing_value = find_impl(*this, key)) {
+            *existing_value = std::move(result);
+            return *existing_value;
+        }
+        auto& entry = buffer_[write_index_];
+        entry.first = key;
+        entry.second = std::move(result);
+
+        write_index_ = (write_index_ + 1) % N;
+        if (size_ == N) {
+            read_index_ = (read_index_ + 1) % N;  // overwrite last entry
+        } else {
+            size_++;
+        }
+        return entry.second;
+    }
+
+    const T* find(Key key) const noexcept {
+        return find_impl(*this, key);
+    }
+
+private:
+    template <typename Self>
+    static auto* find_impl(Self&& self, Key key) noexcept {
+        for (std::size_t i = 0; i < self.size(); ++i) {
+            const auto index = (self.read_index_ + i) % N;
+            auto& entry = self.buffer_[index];
+            if (entry.first == key) {
+                return &entry.second;
+            }
+        }
+        // TODO: find cleaner solution
+        if constexpr (std::is_const_v<std::remove_reference_t<Self>>) {
+            return static_cast<const T*>(nullptr);
+        } else {
+            return static_cast<T*>(nullptr);
+        }
+    }
+
+    std::array<std::pair<Key, T>, N> buffer_{};
+    std::size_t size_{0};
+    std::size_t write_index_{0};
+    std::size_t read_index_{0};
 };
 
 template <template <typename> typename Storage, typename... Ts>
@@ -52,16 +96,27 @@ public:
     template <typename T>
     auto& insert(CacheKey key, T&& result) {
         using ValueType = std::remove_cvref_t<T>;
+        assertIsSupportedType<ValueType>();
         return storage<ValueType>().insert(key, std::forward<T>(result));
     }
 
     template <typename T>
     const T* find(CacheKey key) const noexcept {
         using ValueType = std::remove_cvref_t<T>;
+        assertIsSupportedType<ValueType>();
         return storage<ValueType>().find(key);
     }
 
 private:
+    template <typename T>
+    static constexpr void assertIsSupportedType() {
+        static_assert(
+            (std::is_same_v<T, Ts> || ...),
+            "Type not supported for caching. "
+            "You may want to add the type to the type list of the Cache definition."
+        );
+    }
+
     template <typename T>
     constexpr Storage<T>& storage() noexcept {
         static_assert(std::is_same_v<T, std::remove_cvref_t<T>>);
@@ -77,7 +132,10 @@ private:
     std::tuple<Storage<Ts>...> caches_;
 };
 
-struct Cache : public BasicCache<SingleCacheEntryStorage, bool, int, std::size_t, float, double> {};
+template <typename T>
+using Storage = RingBufferStorage<CacheKey, T, 16>;
+
+struct Cache : public BasicCache<Storage, bool, int, std::size_t, float, double> {};
 
 // TODO: avoid expensive copies with proxy object for cached values.
 template <typename Cache, typename Func, typename... Args>
--- tests/test_common.cpp
+++ tests/test_common.cpp
@@ -12,6 +12,51 @@ static int increment(int x) {
     return x + 1;
 }
 
+TEST_CASE("RingBufferStorage") {
+    openae::RingBufferStorage<int, float, 3> storage;
+    CHECK(storage.size() == 0);
+
+    SECTION("insert with same key") {
+        CHECK(storage.insert(1, 1.1f) == 1.1f);
+        CHECK(storage.size() == 1);
+
+        CHECK(storage.insert(1, 2.2f) == 2.2f);
+        CHECK(storage.size() == 1);
+    }
+
+    SECTION("insert with different keys") {
+        CHECK(storage.insert(1, 1.1f) == 1.1f);
+        CHECK(storage.size() == 1);
+
+        CHECK(storage.insert(2, 2.2f) == 2.2f);
+        CHECK(storage.size() == 2);
+    }
+
+    SECTION("insert with overflow") {
+        storage.insert(1, {});
+        CHECK(storage.size() == 1);
+        storage.insert(2, {});
+        CHECK(storage.size() == 2);
+        storage.insert(3, {});
+        CHECK(storage.size() == 3);
+        storage.insert(4, {});
+        CHECK(storage.size() == 3);
+
+        CHECK(storage.find(1) == nullptr);
+        CHECK(storage.find(2) != nullptr);
+        CHECK(storage.find(3) != nullptr);
+        CHECK(storage.find(4) != nullptr);
+    }
+
+    SECTION("find") {
+        CHECK(storage.find(1) == nullptr);
+
+        CHECK(storage.insert(1, 1.1f) == 1.1f);
+        CHECK(storage.find(1) != nullptr);
+        CHECK(*storage.find(1) == 1.1f);
+    }
+}
+
 TEST_CASE("Cache") {
     auto cache = openae::make_cache();
     CHECK(square(2) == 4);
