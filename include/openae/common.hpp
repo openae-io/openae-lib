1649baf2282b9b8de59b749ad80eba412e2af0b9
Lukas Berbuer <lukas.berbuer@gmail.com>
2025-01-21 16:13:50 +0100

    refactor: reorder Env members

--- include/openae/common.hpp
+++ include/openae/common.hpp
@@ -6,12 +6,6 @@
 
 namespace openae {
 
-using MemoryResource = std::pmr::memory_resource;
-
-struct Cache;
-
-std::unique_ptr<Cache, void(*)(Cache*)> make_cache();
-
 enum class LogLevel {
     Trace = 0,
     Debug,
@@ -23,11 +17,17 @@ enum class LogLevel {
 
 using Logger = std::function<void(LogLevel, const char*)>;
 
+using MemoryResource = std::pmr::memory_resource;
+
+struct Cache;
+
+std::unique_ptr<Cache, void(*)(Cache*)> make_cache();
+
 struct Env {
-    MemoryResource* mem_resource = nullptr;
-    Cache* cache = nullptr;
     LogLevel log_level = LogLevel::Info;
     Logger logger = nullptr;
+    MemoryResource* mem_resource = nullptr;
+    Cache* cache = nullptr;
 };
 
 void log(Env& env, LogLevel level, const char* msg);
